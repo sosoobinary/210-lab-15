78d893e8ff589fa617ecafa2b9db3b887e9de846
sosoobinary <quangxngo04@gmail.com>
2025-09-28 20:49:53 -0700

    10 min

--- moviesCreator.cpp
+++ moviesCreator.cpp
@@ -1,1 +1,39 @@
-#include iostream
+// COMSC-210 | Lab 15 | Quang Ngo
+// IDE used: Visual Studio Code
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+class Movie {
+    private:
+        string screenWriter;
+        int year;
+        string title;
+
+    public:
+        void setScreenWriter(const string& s) {
+            screenWriter = s;
+        }
+        void setYear(const string& y) {
+            year = y;
+        }
+        void setTitle(const string& t) {
+            title = t;
+        }
+
+        string getScreenWriter() const {
+            return screenWriter;
+        }
+        int getYear() const {
+            return year;
+        }
+        string getTitle() const {
+            return title;
+        }
+
+};
+
+int main() {
+
+}
